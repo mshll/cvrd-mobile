4471b1978a5c849905175351d275f77779eb7e8e
jannakam <jannakalmuqaisib@gmail.com>
2025-01-27 14:00:32 +0300

    add card animation and cards

--- src/navigation/paths.js
+++ src/navigation/paths.js
@@ -1,14 +1,21 @@
 export const Paths = {
-  // Main paths
-  HOME: 'Home',
-  ACTIVITY: 'Activity',
-  ADD_CARD: 'AddCard',
-  SUBSCRIPTIONS: 'Subscriptions',
-  PROFILE: 'Profile',
-  CARD_DETAILS: 'CardDetails',
+  // Tab paths
+  HOME: "Home",
+  ACTIVITY: "Activity",
+  ADD_CARD: "AddCard",
+  SUBSCRIPTIONS: "Subscriptions",
+  PROFILE: "Profile",
+  CARD_DETAILS: "CardDetails",
+
+  // Stack screen paths
+  HOME_SCREEN: "HomeScreen",
+  ACTIVITY_SCREEN: "ActivityScreen",
+  SUBSCRIPTIONS_SCREEN: "SubscriptionsScreen",
+  PROFILE_SCREEN: "ProfileScreen",
+  ADD_CARD_SCREEN: "AddCardScreen",
 
   // Auth paths
-  ONBOARDING: 'Onboarding',
-  LOGIN: 'Login',
-  SIGNUP: 'Signup',
+  ONBOARDING: "Onboarding",
+  LOGIN: "Login",
+  SIGNUP: "Signup",
 };
