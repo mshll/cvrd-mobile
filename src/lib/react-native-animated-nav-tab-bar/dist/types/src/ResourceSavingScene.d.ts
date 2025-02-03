d961982ad666fdcc8a3f4569e68b9fdeab056918
meshal <imeshalfm@gmail.com>
2025-02-03 15:04:24 +0300

    rm old navbar

--- src/lib/react-native-animated-nav-tab-bar/dist/types/src/ResourceSavingScene.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-type Props = {
-    isVisible: boolean;
-    children: React.ReactNode;
-    enabled?: boolean;
-    style?: any;
-};
-export default function ResourceSavingScene({ isVisible, children, style, ...rest }: Props): React.JSX.Element;
-export {};
--- src/lib/react-native-animated-nav-tab-bar/dist/types/src/UIComponents.d.ts
+++
@@ -1,6 +0,0 @@
-declare const BottomTabBarWrapper: any;
-declare const TabButton: any;
-declare const Label: any;
-declare const Dot: any;
-declare const SHADOW: any;
-export { BottomTabBarWrapper, TabButton, Label, Dot, SHADOW };
